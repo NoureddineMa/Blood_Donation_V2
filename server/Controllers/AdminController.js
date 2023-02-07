636e195cbd5101009d96e8542cde1e296813ae86
NoureddineMa <noureddinemaher13@gmail.com>
2023-02-07 09:17:38 +0100

    ADD fichier AuthController & Set up tailwind Css

--- server/Controllers/AdminController.js
+++ server/Controllers/AdminController.js
@@ -1,10 +1,3 @@
 const asyncHandler = require('express-async-handler')
 const Admin = require('../Models/AdminModel')
 
-const AdminRegister = asyncHandler((req,res) => {
-    res.send('AdminRegister')
-})
-
-const AdminLogin = asyncHandler((req,res) => {
-    res.send('AdminLogin')
-})
