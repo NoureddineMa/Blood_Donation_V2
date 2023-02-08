dd99c17ccc21c0745291ffe406ffea6206700c1c
NoureddineMa <noureddinemaher13@gmail.com>
2023-02-08 11:19:31 +0100

    Functionnality Forget Password  Done

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -10,25 +10,25 @@ const expressAsyncHandler = require('express-async-handler')
 // @Route :api/auth/register
 // *** acces : public ***
 
-const Register = asyncHandler(async (req,res) => {
-    const {First_Name, Second_Name , Email , Password , _roles} = req.body;
+const Register = asyncHandler(async (req, res) => {
+    const { First_Name, Second_Name, Email, Password, _roles } = req.body;
 
-    if(!First_Name || !Second_Name || !Email || !Password || !_roles){
-            res.status(400)
-            .json({message: "please fill all fields"})
+    if (!First_Name || !Second_Name || !Email || !Password || !_roles) {
+        res.status(400)
+            .json({ message: "please fill all fields" })
     }
     // check if the User Alreasy Exist 
-    const userExist = await User.findOne({Email})
-    if(userExist) {
+    const userExist = await User.findOne({ Email })
+    if (userExist) {
         res.status(400)
-        .json({message: "User Already Exist "})
+            .json({ message: "User Already Exist " })
     }
 
     // hash Password : 
     const salt = await bcrypt.genSalt(10)
     const HashedPassword = await bcrypt.hash(Password, salt)
     // find Roles By {id}
-    const RoleID = await Roles.findOne({_roles: req.body._roles})
+    const RoleID = await Roles.findOne({ _roles: req.body._roles })
     const RoleName = RoleID._id
     // Create a User : 
     const user = await User.create({
@@ -38,35 +38,35 @@ const Register = asyncHandler(async (req,res) => {
         Password: HashedPassword,
         _roles: RoleName
     })
-    if(user){
+    if (user) {
         res.status(200)
-        .json({message: "User Created Succesfully But First You need to Verify Your Email !"})
-    // Create MailTransporter:
-    const transporter = nodemailer.createTransport({
-        service: process.env.SMTP,
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PWD_EMAIL
-        },
-        tls: {
-            rejectUnauthorized: false
-        }        
-    })
-    const token = jwt.sign({_id:user._id }, process.env.JWT_SECRET, {expiresIn: '2h'})
-    // Create BodyMail
-    const mailContent = {
-        from : "Verify Your Email" + process.env.EMAIL,
-        to: Email,
-        subject: "Verify Your Email",
-        html: `<h2>Hi Please Verify Your Email <a href="http://localhost:4000/register/verify/${token}"> <button>Click here</button></a></h2>`
-    }
-    // Send Mail:
-    transporter.sendMail(mailContent, (err) => !err)
-     // Other Scénario When User Cant Register
+            .json({ message: "User Created Succesfully But First You need to Verify Your Email !" })
+        // Create MailTransporter:
+        const transporter = nodemailer.createTransport({
+            service: process.env.SMTP,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PWD_EMAIL
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        })
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '2h' })
+        // Create BodyMail
+        const mailContent = {
+            from: "Verify Your Email" + process.env.EMAIL,
+            to: Email,
+            subject: "Verify Your Email",
+            html: `<h2>Hi Please Verify Your Email <a href="http://localhost:4000/register/verify/${token}"> <button>Click here</button></a></h2>`
+        }
+        // Send Mail:
+        transporter.sendMail(mailContent, (err) => !err)
+        // Other Scénario When User Cant Register
     }
     else {
         res.status(400)
-        .json({message: "Invalid User Data"})
+            .json({ message: "Invalid User Data" })
     }
 })
 
@@ -74,43 +74,43 @@ const Register = asyncHandler(async (req,res) => {
 // @Route :api/auth/login
 // *** acces : public ***
 
-const Login = asyncHandler(async (req,res) => {
-    const {Email , Password} = req.body
+const Login = asyncHandler(async (req, res) => {
+    const { Email, Password } = req.body
 
 
-    if(!Email || !Password){
+    if (!Email || !Password) {
         res.status(400)
-        .json({message: "Please fill all fields"})
+            .json({ message: "Please fill all fields" })
     }
 
     // Check for userEmail :
-    const user = await User.findOne({Email})
-    if(user){
+    const user = await User.findOne({ Email })
+    if (user) {
         const StatusUser = user.Verified
-        if(StatusUser){
+        if (StatusUser) {
             userRole = user._roles
-            const findRoleByID = await Roles.findById({_id: userRole})
+            const findRoleByID = await Roles.findById({ _id: userRole })
             const nameRole = findRoleByID.role
-            if(user && (await bcrypt.compare(Password , user.Password))){
+            if (user && (await bcrypt.compare(Password, user.Password))) {
 
                 // CREATE TOKEN
-                const token = jwt.sign({_id: user._id} , process.env.JWT_SECRET , {
+                const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
                     expiresIn: '24h'
                 });
                 res.status(200)
-                .json(token , nameRole , user)
+                    .json(token, nameRole, user)
             } else {
                 res.status(400)
-                .json({message: "Invalid Credentials"})
+                    .json({ message: "Invalid Credentials" })
             }
         }
         else {
             res.status(400)
-            .json({message: "You need to verify Your Email to Login !"})
+                .json({ message: "You need to verify Your Email to Login !" })
         }
     } else {
         res.status(400)
-        .json({message: "User Not Found !"})
+            .json({ message: "User Not Found !" })
     }
 })
 
@@ -119,31 +119,70 @@ const Login = asyncHandler(async (req,res) => {
 // @Route /register/verify/:token
 // *** acces : private ***
 
-const EmailVerification = expressAsyncHandler(async (req,res) => {
-     // retrieve token from params 
-     const token = req.params.token
-     // Verify token :
-     const userData = jwt.verify(token , process.env.JWT_SECRET)
-     if(!userData) return res.status(500)
-     .json({
-        succes: false,
-        message: 'NO TOKEN'
-    })
+const EmailVerification = expressAsyncHandler(async (req, res) => {
+    // retrieve token from params 
+    const token = req.params.token
+    // Verify token :
+    const userData = jwt.verify(token, process.env.JWT_SECRET)
+    if (!userData) return res.status(500)
+        .json({
+            succes: false,
+            message: 'NO TOKEN'
+        })
     // GET ID 
     const userID = userData._id
-    User.updateOne({_id: userID} , {$set : {Verified : true }})
-    .then(() => {
-        res.status(200)
-        .json({message: "Email Verified Succesfully !"})
-    }).catch((err) => {
-        res.status(400)
-        .json({message: "Something Went Wrong" + err})
-    })
+    User.updateOne({ _id: userID }, { $set: { Verified: true } })
+        .then(() => {
+            res.status(200)
+                .json({ message: "Email Verified Succesfully !" })
+        }).catch((err) => {
+            res.status(400)
+                .json({ message: "Something Went Wrong" + err })
+        })
 })
+
+
+// *** *** *** method :post *** *** ***
+// @Route :api/auth/forgetpassword
+// *** acces : public ***
+
+const ForegetPassword = asyncHandler(async(req,res) => {
+    const UserEmail = req.body.Email
+    const RetrieveEmail = await User.findOne({Email: UserEmail})
+    if(RetrieveEmail){
+        const transporter = nodemailer.createTransport({
+            service: process.env.SMTP,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PWD_EMAIL
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        })
+        const token = jwt.sign({_id: RetrieveEmail._id} , process.env.JWT_SECRET, {expiresIn: '10m'})
+        // Create MailBody
+        const mailContent = {
+            from: "Reset Password" + process.env.EMAIL,
+            to: UserEmail,
+            subject: 'Reset Password',
+            html: `<h2>To reset you password please click here <a href="http://localhost:4000/resetpassword/${token}">here</a></h2>`
+        }
+        // Send Email : 
+        transporter.sendMail(mailContent , (err) => !err ? console.log(`Mail just send to ${UserEmail}`) : console.log(err))
+        res.status(200)
+        .json({message:`Mail just send to ${UserEmail}`})
+    } else {
+        res.status(400)
+        .json({message: "Email Wrong Or Not Found !!"})
+    }
+})
+
 
 
 module.exports = {
     Register,
     Login,
-    EmailVerification
+    EmailVerification,
+    ForegetPassword
 }
--- server/Routers/AuthRouter.js
+++ server/Routers/AuthRouter.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-const {Register , Login , EmailVerification} = require('../Controllers/AuthController')
+const {
+    Register,
+    Login,
+    EmailVerification,
+    ForegetPassword
+    } = require('../Controllers/AuthController')
 
 
 router.post('/register', Register)
 router.post('/login' , Login)
 router.get('/register/verify/:token' , EmailVerification)
+router.post('/forgetpassword' , ForegetPassword)
 
 module.exports = router
