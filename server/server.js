620bdb2b2655c877eb4a05b8ef3893ae2748a77e
NoureddineMa <noureddinemaher13@gmail.com>
2023-02-14 22:02:58 +0100

    cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const initDb = require('./Config/initDb')
 const AuthRouter = require('./Routers/AuthRouter')
 initDb();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT_APP || 5000
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/api/auth', AuthRouter)
+app.use(cors({origin:true,credentials:true}))
 
 app.listen(PORT , () => {
     console.log(`Server Running Under Port : ${PORT}`);
