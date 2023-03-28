885f63528f20e0f946e4a5146d9d09cea4e32e57
NoureddineMa <noureddinemaher13@gmail.com>
2023-03-28 09:55:01 +0200

    add middleware check token

---
+++ server/Middlewares/verifyToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+const verifyToken = function(req,res,next){
+
+    const token = req.header('authorization').split(' ')[1]
+    if(!token) return res.status(401).json({message: "Acces Denied !"})
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified
+        next()
+    } catch (error) {
+        res.status(400)
+        .json({message: "Invalid Token"})
+    }}
+
+module.exports = verifyToken;
