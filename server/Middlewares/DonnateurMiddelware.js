e528b5cbe6681f94ea923c8d5c3034c945ebc8e0
NoureddineMa <noureddinemaher13@gmail.com>
2023-04-06 22:43:00 +0200

    fix middelwares

--- server/Middlewares/DonnateurMiddelware.js
+++ server/Middlewares/DonnateurMiddelware.js
@@ -1,19 +1,18 @@
-// const asyncHandler = require('express-async-handler')
-// const Donnateur = require('../Models/DonnationRequestModel')
-// const Roles = require('../Models/RoleModel')
+const asyncHandler = require('express-async-handler')
+const User = require('../Models/UserModel')
+const Roles = require('../Models/RoleModel')
 
-// const DonnateurMiddleware = asyncHandler(async(req,res,next) => {
-//     const donnateur = await Donnateur.findById({_id: req.user._id})
-//     const roleID = donnateur?._roles
-//     const SchemaRole = await Roles.findById({_id: roleID})
-//     console.log(SchemaRole);
-//     const nameRole = SchemaRole.role
-//     console.log(nameRole);
-//     if(nameRole !== 'Donnateur'){
-//         res.status(400)
-//         .json({message: "ACCES DENIED !!"})
-//     }
-//     next()
-// })
+const DonnateurMiddleware = asyncHandler(async (req, res, next) => {
+    console.log(req.user._id);
+    const donnateur = await User.findById({ _id: req.user._id })
+    const roleID = donnateur?._roles
+    const SchemaRole = await Roles.findById({ _id: roleID })
+    const nameRole = SchemaRole.role
+    if (nameRole !== 'Donnateur') {
+        return res.status(400)
+            .json({ message: "ACCES DENIED !!" })
+    }
+    next()
+})
 
-// module.exports = DonnateurMiddleware
+module.exports = DonnateurMiddleware    
--- server/Middlewares/verifyToken.js
+++ server/Middlewares/verifyToken.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken')
 
-const verifyToken = function(req,res,next){
+const verifyToken = function (req, res, next) {
 
-    const token = req.header('authorization').split(' ')[1]
-    if(!token) return res.status(401).json({message: "Acces Denied !"})
-
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1]
+    if (!token || undefined) return res.status(401).json({ message: "Acces Denied !" })
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified
         next()
     } catch (error) {
         res.status(400)
-        .json({message: "Invalid Token"})
-    }}
+            .json({ message: "Invalid Token" })
+    }
+}
 
 module.exports = verifyToken;
